5659aa89be1690d1cb70c6b22851a0d4f52ff4bf
Tanzim <tanzim008@gmail.com>
2023-01-22 23:26:42 -0330

    Normal Function

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const Buttons = document.querySelectorAll('.countryButton');
-const wIinfo = document.querySelectorAll('.w-info');
-Buttons.forEach(button =>{
-    button.addEventListener('click', ()=>{
-        wIinfo.addEventListener('click',()=>{
-            
-        })
-    })
-})
+// const Buttons = document.querySelectorAll('.countryButton');
+// const wIinfo = document.querySelectorAll('.w-info');
+// Buttons.forEach(button =>{
+//     button.addEventListener('click', ()=>{
+//         wIinfo.addEventListener('click',()=>{
+//             WInfo
+//         })
+//     })
+// })
+
+
+// synchronous
+function one(){
+    console.log(" Step 1 ");
+}
+
+function two(){
+    console.log(" Step 2 ");
+}
+
+one();
+two();
